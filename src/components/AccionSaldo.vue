a8afc6f324492296971d26b2863d471532c79453
Gabriel PD <omsystem.gabriel@gmail.com>
2021-06-15 02:47:12 +0100

    Add files via upload

---
+++ src/components/AccionSaldo.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="aumentar"> {{ texto }}</button>
+</template>
+
+<script>
+export default {
+    props:{
+        texto: String
+    },
+    methods: {
+        aumentar(){
+            this.$emit('accion')
+        }
+    }
+}
+</script>
+
+<style>
+    button{
+        margin: 1em
+    }
+</style>
---
+++ src/components/Cuenta.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>Cuenta</h1>
+    <h3>Saldo {{cantidad}}</h3>
+    <AccionSaldo texto="Aumentar" @accion="aumentar"/>
+    <AccionSaldo texto="Disminuir" :disabled="desactivar" @accion="disminuir"/>
+</template>
+
+<script>
+import AccionSaldo from "./AccionSaldo.vue";
+export default {
+    components: {
+        AccionSaldo
+    },
+    name: "Cuenta",
+    data() {
+        return {
+            cantidad: 1000,
+            desactivar: false
+        }
+    },
+    methods: {
+        aumentar(){
+            this.cantidad = this.cantidad + 100
+        },
+        disminuir() {
+            this.cantidad = this.cantidad - 100
+        }
+    },
+    updated(){
+        if(this.cantidad === 0){
+            this.desactivar = true
+        }else{
+            this.desactivar = false
+        }
+    }  
+    
+}
+</script>
+
+<style scoped>
+    h1 {
+        color: peru;
+    }
+</style>
